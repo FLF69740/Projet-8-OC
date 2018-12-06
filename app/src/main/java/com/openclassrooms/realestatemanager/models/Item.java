6c43a7bed6ee1381420a7632737190b0a01bc9ac
francois LE FORT <lefort.francois_pro@yahoo.com>
2018-12-06 15:53:26 +0100

    creation of Room tables : User and Item

---
+++ app/src/main/java/com/openclassrooms/realestatemanager/models/Item.java
@@ -0,0 +1,141 @@
+package com.openclassrooms.realestatemanager.models;
+
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.ForeignKey;
+import android.arch.persistence.room.PrimaryKey;
+import java.util.List;
+
+@Entity(foreignKeys = @ForeignKey(entity = User.class, parentColumns = "mId", childColumns = "mUserId"))
+public class Item {
+
+    @PrimaryKey(autoGenerate = true) private long mId;
+    private String mType;
+    private int mPrice;
+    private int mDimension;
+    private int mRoomNumber;
+    private String mDescription;
+    private List<String> mUrlPicture;
+    private String mAdress;
+    private List<String> mPoInterest;
+    private Boolean mSold;
+    private String mDateInscription;
+    private String mDateSold;
+    private long mUserId;
+
+    public Item(long id, String type, int price, String adress, String dateInscription, long userId) {
+        this.mId = id;
+        this.mType = type;
+        this.mPrice = price;
+        this.mAdress = adress;
+        this.mDateInscription = dateInscription;
+        this.mUserId = userId;
+    }
+
+    /**
+     *  GETTER AND SETTER
+     */
+
+    public long getId() {
+        return mId;
+    }
+
+    public void setId(long id) {
+        mId = id;
+    }
+
+    public String getType() {
+        return mType;
+    }
+
+    public void setType(String type) {
+        mType = type;
+    }
+
+    public int getPrice() {
+        return mPrice;
+    }
+
+    public void setPrice(int price) {
+        mPrice = price;
+    }
+
+    public int getDimension() {
+        return mDimension;
+    }
+
+    public void setDimension(int dimension) {
+        mDimension = dimension;
+    }
+
+    public int getRoomNumber() {
+        return mRoomNumber;
+    }
+
+    public void setRoomNumber(int roomNumber) {
+        mRoomNumber = roomNumber;
+    }
+
+    public String getDescription() {
+        return mDescription;
+    }
+
+    public void setDescription(String description) {
+        mDescription = description;
+    }
+
+    public List<String> getUrlPicture() {
+        return mUrlPicture;
+    }
+
+    public void setUrlPicture(List<String> urlPicture) {
+        mUrlPicture = urlPicture;
+    }
+
+    public String getAdress() {
+        return mAdress;
+    }
+
+    public void setAdress(String adress) {
+        mAdress = adress;
+    }
+
+    public List<String> getPoInterest() {
+        return mPoInterest;
+    }
+
+    public void setPoInterest(List<String> poInterest) {
+        mPoInterest = poInterest;
+    }
+
+    public Boolean getSold() {
+        return mSold;
+    }
+
+    public void setSold(Boolean sold) {
+        mSold = sold;
+    }
+
+    public String getDateInscription() {
+        return mDateInscription;
+    }
+
+    public void setDateInscription(String dateInscription) {
+        mDateInscription = dateInscription;
+    }
+
+    public String getDateSold() {
+        return mDateSold;
+    }
+
+    public void setDateSold(String dateSold) {
+        mDateSold = dateSold;
+    }
+
+    public long getUserId() {
+        return mUserId;
+    }
+
+    public void setUserId(long userId) {
+        mUserId = userId;
+    }
+}
---
+++ app/src/main/java/com/openclassrooms/realestatemanager/models/User.java
@@ -0,0 +1,56 @@
+package com.openclassrooms.realestatemanager.models;
+
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+
+@Entity
+public class User {
+
+    @PrimaryKey(autoGenerate = true) private Long mId;
+    private String mUsername;
+    private String mUrlPicture;
+    private String mUserDescription;
+
+    public User(Long id, String username, String urlPicture, String userDescription) {
+        this.mId = id;
+        this.mUsername = username;
+        this.mUrlPicture = urlPicture;
+        this.mUserDescription = userDescription;
+    }
+
+    /**
+     *  GETTER AND SETTER
+     */
+
+    public Long getId() {
+        return mId;
+    }
+
+    public void setId(Long id) {
+        mId = id;
+    }
+
+    public String getUsername() {
+        return mUsername;
+    }
+
+    public void setUsername(String username) {
+        mUsername = username;
+    }
+
+    public String getUrlPicture() {
+        return mUrlPicture;
+    }
+
+    public void setUrlPicture(String urlPicture) {
+        mUrlPicture = urlPicture;
+    }
+
+    public String getUserDescription() {
+        return mUserDescription;
+    }
+
+    public void setUserDescription(String userDescription) {
+        mUserDescription = userDescription;
+    }
+}
